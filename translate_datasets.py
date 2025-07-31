7837bd5a056e4489f7d428ffe3d4327f1cf69013
Jon Liu <your email>
2025-07-31 10:56:01 -0700

    Added translation code

---
+++ translate_datasets.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from datasets import load_dataset
+import openai
+import time
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+ds = load_dataset("google/boolq", split="train[:5]")
+df = ds.to_pandas()
+
+def translate_text(text, source_dial="English", target_dial="Chicano English"):
+    print(f"Translating example {1}/{len(ds)}")
+    response = openai.chat.completions.create(
+        model="ft:gpt-4o-2024-08-06:algoverse:chicano-translator:BzAehkfn",
+        messages=[
+            {"role": "system", "content": f"You are a translation assistant. Translate from {source_dial} to {target_dial}."},
+            {"role": "user", "content": text}
+        ],
+        temperature = 0.3
+    )
+    return response.choices[0].message.content.strip()
+
+df["question"] = df["question"].apply(lambda x: translate_text(x))
+df["passage"] = df["passage"].apply(lambda x: translate_text(x))
+df.to_csv("Chicano_boolq.csv", index=False)
