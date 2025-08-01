93ca0823483a625655643d1d8e731ebd88c038aa
Jon Liu <your email>
2025-08-01 10:54:52 -0700

    removed fine tuning files

--- check_ft_status.py
+++
@@ -1,8 +0,0 @@
-import openai
-import os
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-job = openai.fine_tuning.jobs.retrieve("ftjob-jM4TQTuyOR20d0oqPmqUpn2x")
-print(job.status)
-print("Model ID:", job.fine_tuned_model)
--- translate_datasets.py
+++ translate_datasets.py
@@ -6,15 +6,34 @@ import os
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-ds = load_dataset("google/boolq", split="train[:5]")
+ds = load_dataset("google/boolq", split="train")
 df = ds.to_pandas()
+
+few_shot_prompt = """
+Translate the following from Standard American English to Chicano English
+
+SAE: You guys
+CE: y'all
+
+SAE: caught
+CE: catched
+
+SAE: He went
+CE: he had went
+
+SAE: They're all in there, aren't they
+CE: They're all in there, ain't they
+
+SAE: He doesn't like me
+CE: He don't like me
+"""
 
 def translate_text(text, source_dial="English", target_dial="Chicano English"):
     response = openai.chat.completions.create(
-        model="ft:gpt-4o-2024-08-06:algoverse:chicano-translator:BzAehkfn",
+        model="gpt-4o",
+        full_prompt = few_shot_prompt + f"\nSAE: \"{text}\"\nCE:"
         messages=[
-            {"role": "system", "content": f"You are a translation assistant. Translate from {source_dial} to {target_dial}."},
-            {"role": "user", "content": text}
+            {"role": "user", "content": f"Translate from {source_dial} to {target_dial}."},
         ],
         temperature = 0.3
     )
