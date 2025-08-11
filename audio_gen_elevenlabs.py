010962ef61851ab133435a12140187a1e58b024f
Jon Liu <your email>
2025-08-11 11:14:48 -0700

    created audio samples

--- audio_gen_elevenlabs.py
+++ audio_gen_elevenlabs.py
@@ -1,25 +1,26 @@
 from elevenlabs.client import ElevenLabs
 import os
 import pandas as pd
+from dotenv import load_dontenv
 
-client = ElevenLabs(api_key="enter api key here!!!!")#replace with your api key
+load_dotenv()
 
-df = pd.read_excel("/Users/priynshuaggarwal/Documents/Algoverse/boolq_jamaican_english.xlsx")#location for excel file
+df = pd.read_excel("./INDIAN_ENGLISH/boolq_indian_english.xlsx")#location for excel file
 
 
-for i in range(20):
-    question = df.iloc[i, 1] #column 2, but python = 1 (indexing)
+for i in range(4,5):
+    question = df.iloc[i, 3] #column 2, but python = 1 (indexing)
     
 
     audio = client.text_to_speech.convert(
         text=question,
-        voice_id="dhwafD61uVd8h85wAZSE",  # Jamaican English voice id
+        voice_id="vO7hjeAjmsdlGgUdvPpe",  # Jamaican English voice id
         model_id="eleven_multilingual_v2",
         output_format="mp3_44100_128"
     )
     
 
-    save_path = f"/Users/priynshuaggarwal/Documents/Algoverse/jamaican_audio/question_{i+1}.mp3" #saving each audio file with a unique name
+    save_path = f"./INDIAN_ENGLISH/passage_{i+1}.mp3" #saving each audio file with a unique name
     with open(save_path, "wb") as f:
         for chunk in audio:
             f.write(chunk)
