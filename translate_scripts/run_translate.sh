f4df5eb03ac2d15b9626623073220325a00a724a
Jon Liu <jonliu4242@gmail.com>
2025-08-26 16:22:15 -0700

    Sorted translation by dataset type. Added translate_mc.py

---
+++ translate_scripts/run_translate.sh
@@ -0,0 +1,12 @@
+# Arguments that aren't required
+# no_passage is set as true as default, model is gpt-4o, --temp is 0.3
+NOT_REQUIRED_COMMON_ARGS=' --model_name --temp'
+NOT_REQUIRED_PQA_ARGS='--no_passage'
+
+# Required arguments
+REQUIRED_COMMON_ARGS='--dataset --target_dial --output_dir'
+REQUIRED_MC_ARGS='--n_choices --no_premise'
+
+# Run audio generation script with required arguments and any desired unrequired ones
+py translate_pqa.py 
+py translate_mc.py
